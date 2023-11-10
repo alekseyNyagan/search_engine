ec8faa2415da3caf7b3f95a15c155a2ec98031fb
alekseyNyagan <alexei.muraschkin@mail.ru>
2023-11-10 10:45:06 +0500

    snippet fixed and kafka elk pipeline for logging added

--- src/main/java/main/service/SearchSystemServiceImpl.java
+++ src/main/java/main/service/SearchSystemServiceImpl.java
@@ -10,43 +10,51 @@ import main.model.Lemma;
 import main.model.Page;
 import main.repository.LemmaRepository;
 import main.repository.PageRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.morphology.WrongCharaterException;
 import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service
 public class SearchSystemServiceImpl implements SearchSystemService {
 
     private final static int RESULTS_ON_PAGE = 10;
-
+    private final static int SNIPPET_LENGTH = 50;
     private final LemmaRepository lemmaRepository;
     private final PageRepository pageRepository;
+    private List<Page> pages;
+    private Map<Page, Float> relativeRelevance;
+    private final Lemmatizator lemmatizator;
+    private final static Logger LOGGER = LogManager.getLogger();
 
     @Autowired
-    public SearchSystemServiceImpl(LemmaRepository lemmaRepository, PageRepository pageRepository) {
+    public SearchSystemServiceImpl(LemmaRepository lemmaRepository, PageRepository pageRepository) throws IOException {
         this.lemmaRepository = lemmaRepository;
         this.pageRepository = pageRepository;
+        pages = new ArrayList<>();
+        relativeRelevance = new HashMap<>();
+        lemmatizator = new Lemmatizator();
     }
 
     @Override
-    public AbstractResponse search(String query, String site, int offset, int limit) throws IOException {
+    public AbstractResponse search(String query, String site, int offset, int limit) {
+        Set<String> lemmas;
         if (query.isEmpty()) {
             return new ErrorResponse(false, "Задан пустой поисковый запрос");
         } else {
             SearchResponse searchResponse = new SearchResponse();
-            Lemmatizator lemmatizator = new Lemmatizator();
-            StringBuilder snippetString = new StringBuilder();
-            Set<String> lemmas = lemmatizator.getQueryLemmas(query);
+            lemmas = lemmatizator.getQueryLemmas(query);
             TreeSet<Lemma> lemmaSet = new TreeSet<>(Comparator.comparingInt(Lemma::getFrequency));
             lemmaSet.addAll(site == null ? lemmaRepository.findAllByLemmas(lemmas) : lemmaRepository.findAllByLemmasAndSite(lemmas, site));
-            List<Page> pages = getPages(new TreeSet<>(lemmaSet), snippetString);
-            Map<Page, Float> relativeRelevance = getRelevance(pages, lemmaSet);
-            List<PageDTO> dtos = buildPageDTOList(relativeRelevance, pages, snippetString, offset, limit);
+            pages = getPages(new TreeSet<>(lemmaSet));
+            relativeRelevance = getRelevance(pages, new TreeSet<>(lemmaSet));
+            List<PageDTO> dtos = buildPageDTOList(relativeRelevance, pages, new ArrayList<>(lemmas), offset, limit);
 
             searchResponse.setResult(true);
             searchResponse.setCount(pages.size());
@@ -55,40 +63,91 @@ public class SearchSystemServiceImpl implements SearchSystemService {
         }
     }
 
-    private List<PageDTO> buildPageDTOList(Map<Page, Float> relativeRelevance, List<Page> pages, StringBuilder snippetString, int offset, int limit) {
+    private List<PageDTO> buildPageDTOList(Map<Page, Float> relativeRelevance, List<Page> pages, List<String> lemmas, int offset, int limit) {
         List<PageDTO> pageDTOs = new ArrayList<>();
         int pageNumber = offset / RESULTS_ON_PAGE + 1;
-
-        Pattern snippetRegex = Pattern.compile("(\\w+)\\W+" + snippetString + "\\W+(\\w+)");
         for (int i = offset; i < (Math.min(pageNumber * limit, pages.size())); i++) {
             Page page = pages.get(i);
             String title = Jsoup.parse(page.getContent()).title();
-            Matcher snippetMatcher = snippetRegex.matcher(page.getContent().replaceAll("\\<.*?\\>", " "));
-            StringBuilder builder = new StringBuilder();
-            while (snippetMatcher.find()) {
-                String snippet = snippetMatcher.group();
-                builder.append(builder.length() == 0 ? "" : ", ")
-                        .append("<b>")
-                        .append(snippet)
-                        .append("</b>");
-            }
+            String content = Jsoup.clean(page.getContent(), Safelist.basic())
+                    .replaceAll("(<.*?>)|(&nbsp)", " ")
+                    .toLowerCase()
+                    .replaceAll("\\s+", " ")
+                    .trim();
+            String snippet = getSnippet(lemmas, content);
             pageDTOs.add(new PageDTO(page.getSite().getUrl(), page.getSite().getName(), page.getPath()
                     , title
-                    , builder.toString()
+                    , snippet
                     , relativeRelevance.get(page)));
         }
         pageDTOs.sort(Comparator.comparingDouble(PageDTO::getRelevance).reversed());
         return pageDTOs;
     }
 
-    private Map<Page, Float> getRelevance(List<Page> pages, Set<Lemma> lemmaList) {
+    private String getSnippet(List<String> lemmas, String content) {
+        StringBuilder wordsToBold = new StringBuilder();
+        StringBuilder snippet = new StringBuilder();
+
+        for (String lemma : lemmas) {
+            wordsToBold
+                    .append(wordsToBold.length() == 0 ? "" : "|")
+                    .append(lemma);
+        }
+        String lemma = lemmas.get(0);
+        int wordIndex = 0;
+        String[] contentArray = content.split(" ");
+
+        for (String word : contentArray) {
+            try {
+                if (lemmatizator.getWordNormalForm(word).equals(lemma)) {
+                    wordIndex = content.indexOf(word);
+                    break;
+                }
+            } catch (WrongCharaterException exception) {
+                LOGGER.warn(exception.getMessage());
+            }
+        }
+        String snippetSubstring = getSnippetSubstring(content, wordIndex);
+
+        for (String word : snippetSubstring.split(" ")) {
+            try {
+                snippet
+                        .append(snippet.length() == 0 ? "" : " ")
+                        .append(lemmas.contains(lemmatizator.getWordNormalForm(word)) ? "<b>" + word + "</b>" : word);
+            } catch (WrongCharaterException exception) {
+                LOGGER.warn(exception.getMessage());
+            }
+        }
+
+        return snippet.toString();
+    }
+
+    private String getSnippetSubstring(String content, int wordIndex) {
+        String snippetSubstring;
+        int middleOfSnippet = SNIPPET_LENGTH / 2;
+        if (wordIndex - middleOfSnippet <= 0) {
+            int spaceIndex = content.substring(wordIndex, wordIndex + SNIPPET_LENGTH).lastIndexOf(" ");
+            snippetSubstring = content.substring(wordIndex, spaceIndex);
+        } else if (wordIndex + middleOfSnippet >= content.length() - 1) {
+            int spaceIndex = content.substring(wordIndex - SNIPPET_LENGTH).indexOf(" ");
+            snippetSubstring = content.substring(spaceIndex);
+        } else {
+            snippetSubstring = content.substring(wordIndex - middleOfSnippet, wordIndex + middleOfSnippet);
+            int firstSpaceIndex = snippetSubstring.indexOf(" ") + 1;
+            int lastSpaceIndex = snippetSubstring.lastIndexOf(" ");
+            snippetSubstring = snippetSubstring.substring(firstSpaceIndex, lastSpaceIndex);
+        }
+        return snippetSubstring;
+    }
+
+    private Map<Page, Float> getRelevance(List<Page> pages, Set<Lemma> lemmasSet) {
         Map<Page, Float> absoluteRelevance = new HashMap<>();
         Map<Page, Float> relativeRelevance = new HashMap<>();
         float max = 0;
 
         for (Page page : pages) {
             List<Index> indexList = page.getIndex();
-            for (Lemma lemma : lemmaList) {
+            for (Lemma lemma : lemmasSet) {
                 Optional<Index> indexOptional = indexList.stream().filter(index -> index.getLemma() == lemma).findAny();
                 if (indexOptional.isPresent()) {
                     float rank = indexOptional.get().getRank();
@@ -108,30 +167,28 @@ public class SearchSystemServiceImpl implements SearchSystemService {
         return relativeRelevance;
     }
 
-    private List<Page> getPages(TreeSet<Lemma> lemmaSet, StringBuilder snippetString) {
+    private List<Page> getPages(TreeSet<Lemma> lemmaSet) {
         List<Page> pages;
-        Lemma fitstLemma = lemmaSet.pollFirst();
-        if (fitstLemma != null) {
-            snippetString.append(snippetString.length() == 0 ? "" : "|").append(fitstLemma.getLemma());
-            pages = pageRepository.getPagesByLemma(fitstLemma, fitstLemma.getSite());
-            pages.retainAll(searchPagesByNextLemma(pages, lemmaSet, snippetString));
+        Lemma firstLemma = lemmaSet.pollFirst();
+        if (firstLemma != null) {
+            pages = pageRepository.getPagesByLemma(firstLemma, firstLemma.getSite());
+            pages.retainAll(searchPagesByNextLemma(pages, lemmaSet));
         } else {
             pages = Collections.emptyList();
         }
         return pages;
     }
 
-    private List<Page> searchPagesByNextLemma(List<Page> pages, TreeSet<Lemma> lemmaSet, StringBuilder snippetString) {
+    private List<Page> searchPagesByNextLemma(List<Page> pages, TreeSet<Lemma> lemmaSet) {
         Lemma lemma = lemmaSet.pollFirst();
         if (lemma != null) {
-            snippetString.append("|").append(lemma.getLemma());
             List<Page> tempList = new ArrayList<>();
             for (Page page : pages) {
                 if (page.getLemmas().stream().map(Lemma::getLemma).anyMatch(s -> s.equals(lemma.getLemma()))) {
                     tempList.add(page);
                 }
             }
-            searchPagesByNextLemma(tempList, lemmaSet, snippetString);
+            searchPagesByNextLemma(tempList, lemmaSet);
         }
         return pages;
     }
