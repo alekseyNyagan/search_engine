8d94dde86f3a8c9a1ef4452abc1d070d7c08cbf4
aleksey <alexei.muraschkin@mail.ru>
2024-01-18 15:40:13 +0500

    HTMLStorage refactored

--- src/main/java/main/engine/HTMLStorage.java
+++ src/main/java/main/engine/HTMLStorage.java
@@ -21,6 +21,7 @@ public class HTMLStorage {
     private final Site site;
     private final IndexRepository indexRepository;
     private final ApplicationProperties applicationProperties;
+
     public HTMLStorage(FieldRepository fieldRepository, Site site, LemmaRepository lemmaRepository, IndexRepository indexRepository,
                        ApplicationProperties applicationProperties) throws IOException {
         lemmatizator = new Lemmatizator();
@@ -32,11 +33,17 @@ public class HTMLStorage {
     }
 
     public Response getResponse(String url) throws IOException {
-        return Jsoup.connect(url).userAgent(applicationProperties.getUserAgent()).ignoreContentType(true).execute();
+        return Jsoup.connect(url)
+                .userAgent(applicationProperties.getUserAgent())
+                .ignoreContentType(true)
+                .execute();
     }
 
     public Document getDocument(String url) throws IOException {
-        return Jsoup.connect(url).userAgent(applicationProperties.getUserAgent()).ignoreContentType(true).get();
+        return Jsoup.connect(url)
+                .userAgent(applicationProperties.getUserAgent())
+                .ignoreContentType(true)
+                .get();
     }
 
     public void parseHTMLDocument(Page page) {
@@ -44,24 +51,23 @@ public class HTMLStorage {
         List<Field> fields = fieldRepository.findAll();
         Map<String, Float> rankMap = new HashMap<>();
         Set<Index> indexSet = new HashSet<>();
+
         for (Field field : fields) {
             String text = Jsoup.clean(Jsoup.parse(htmlDoc).selectFirst(field.getSelector()).text(), Safelist.basic());
             Map<String, Integer> lemmasMap = lemmatizator.getLemmas(text);
 
             for (String lemma : lemmasMap.keySet()) {
-                float rank = rankMap.containsKey(lemma) ?
-                        rankMap.get(lemma) + (lemmasMap.get(lemma) * field.getWeight()) :
-                        lemmasMap.get(lemma) * field.getWeight();
+                float rank = rankMap.getOrDefault(lemma, 0f) + (lemmasMap.get(lemma) * field.getWeight());
                 rankMap.put(lemma, rank);
             }
-
         }
+
         rankMap.forEach((lemma, rank) -> {
-            Lemma lemmaToInsert = new Lemma(lemma, 1, site);
-            lemmaRepository.save(lemmaToInsert);
-            indexSet.add(new Index(page, lemmaToInsert, rank));
+            Lemma lemmaObject = new Lemma(lemma, 1, site);
+            lemmaRepository.save(lemmaObject);
+            indexSet.add(new Index(page, lemmaObject, rank));
         });
+
         indexRepository.saveAll(indexSet);
     }
 }
-
